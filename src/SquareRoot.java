cc2d2c4f1eb572c22754da9d28b7b36db32dd443
Fan Zhang <fredplusplus@gmail.com>
2013-09-02 23:22:35 -0700

    sqrt

---
+++ src/SquareRoot.java
@@ -0,0 +1,32 @@
+/**
+ * http://leetcode.com/onlinejudge#question_69
+ */
+public class SquareRoot {
+	public static int sqrt(int x) {
+		// Start typing your Java solution below
+		// DO NOT write main() function
+
+		if (x <= 1) {
+			return x;
+		}
+		int left = 0;
+		int right = x;
+		int mid = left + (right - left) / 2;
+		while (right > left) {
+			mid = left + (right - left) / 2;
+			int result = x / mid;
+			if (mid < result) {
+				left = mid;
+			} else if (mid > result) {
+				right = mid;
+			} else {
+				return mid;
+			}
+		}
+		return left;
+	}
+
+	public static void main(String[] args) {
+		System.out.println(sqrt(9));
+	}
+}
