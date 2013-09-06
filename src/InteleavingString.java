6b67b4dc57d10cf5db24bf7283e702b90380a7d4
Fan Zhang <fredplusplus@gmail.com>
2013-09-05 22:27:56 -0700

    q97

---
+++ src/InteleavingString.java
@@ -0,0 +1,59 @@
+/**
+ * http://leetcode.com/onlinejudge#question_97
+ * 
+ * @author fanzhang
+ * 
+ */
+public class InteleavingString {
+	public boolean isInterleave(String s1, String s2, String s3) {
+		// define C[i][j] is true when c[0... i+j-1] is interleaving of a[0..i]
+		// + b[0..j]
+		int len1 = s1.length();
+		int len2 = s2.length();
+		int len3 = s3.length();
+		if (len1 + len2 != len3) {
+			return false;
+		}
+		boolean[][] result = new boolean[len1][len2];
+		// all strings empty case
+		result[0][0] = true;
+
+		for (int i = 0; i < len1; i++) {
+			for (int j = 0; j < len2; j++) {
+				// s1 is empty
+				if (i == 0 && s2.charAt(j) == s3.charAt(j)) {
+					if (j == 0) {
+						result[i][j] = true;
+					} else {
+						result[i][j] = result[i][j - 1];
+					}
+				}
+				// s2 is empty
+				else if (j == 0 && s1.charAt(i) == s3.charAt(i)) {
+					if (i == 0) {
+						result[i][j] = true;
+					} else {
+						result[i][j] = result[i - 1][j];
+					}
+				}
+				// both available, s1 matches s3, s2 doesnt
+				else if (s1.charAt(i) == s3.charAt(i + j) && s2.charAt(j) != s3.charAt(i + j)) {
+					result[i][j] = result[i - 1][j];
+				} else if (s1.charAt(i) != s3.charAt(i + j) && s2.charAt(j) == s3.charAt(i + j)) {
+					result[i][j] = result[i][j - 1];
+				} else if (s1.charAt(i) == s3.charAt(i + j) && s2.charAt(j) == s3.charAt(i + j)) {
+					result[i][j] = result[i][j - 1] || result[i - 1][j];
+				} else {
+					result[i][j] =false;
+				}
+			}
+		}
+		return result[len1-1][len2-1];
+	}
+
+	public static void main(String[] args) {
+		InteleavingString is = new InteleavingString();
+
+		System.out.println(is.isInterleave("a", "", "c"));
+	}
+}
