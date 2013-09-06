ec03990e29cbca0a75e7b1b182e8af9eb4831870
Fan Zhang <fredplusplus@gmail.com>
2013-09-05 23:16:47 -0700

    bugfix

--- src/UniquePath.java
+++ src/UniquePath.java
@@ -20,7 +20,7 @@ public class UniquePath {
 		}
 		for (int i = 1; i < m; i++) {
 			for (int j = 1; j < n; j++) {
-				board[i][j] = board[i - 1][j] + bord[i][j - 1];
+				board[i][j] = board[i - 1][j] + board[i][j - 1];
 			}
 		}
 		return board[m - 1][n - 1];
