38c2b03c61a4253a82dabbd0960652f9b24a9419
Fan Zhang <fredplusplus@gmail.com>
2013-09-07 21:48:18 -0700

    61

---
+++ src/RotateList.java
@@ -0,0 +1,38 @@
+/**
+ * http://leetcode.com/onlinejudge#question_61
+ * 
+ */
+public class RotateList {
+	public ListNode rotateRight(ListNode head, int n) {
+		if (n <= 0 || head == null) {
+			return head;
+		}
+		ListNode probe = head;
+		int size = 0;
+		while (probe != null) {
+			probe = probe.next;
+			size++;
+		}
+		n = n % size;
+		if (n == 0) {
+			return head;
+		}
+		ListNode split = head;
+		for (int i = 0; i < n - 1; i++) {
+			split = split.next;
+		}
+		ListNode newHead = split.next;
+		while (newHead.next != null) {
+			newHead = newHead.next;
+		}
+		newHead.next = head;
+		return newHead;
+	}
+
+	public static void main(String[] args) {
+		ListNode node = new ListNode(1);
+		node.next = new ListNode(2);
+		RotateList rl = new RotateList();
+		System.out.println(rl.rotateRight(node, 1));
+	}
+}
