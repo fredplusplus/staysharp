b6be6d0893a1089251b42acfdb285eea1b1aa17c
Fan Zhang <fredplusplus@gmail.com>
2013-09-07 21:00:32 -0700

    q21

--- src/MergeSortedArray.java
+++ src/MergeSortedArray.java
@@ -1,10 +1,8 @@
-/**
- * http://leetcode.com/onlinejudge#question_88
- * 
- * @author fanzhang
- * 
- */
 public class MergeSortedArray {
+	/**
+	 * http://leetcode.com/onlinejudge#question_88
+	 * 
+	 */
 	public void merge(int A[], int m, int B[], int n) {
 		int pa = m - 1;
 		int pb = n - 1;
@@ -24,4 +22,42 @@ public class MergeSortedArray {
 		}
 	}
 
+	/**
+	 * http://leetcode.com/onlinejudge#question_21
+	 */
+	public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+		if (l1 == null) {
+			return l2;
+		} else if (l2 == null) {
+			return l1;
+		} else {
+			ListNode l = null;
+			ListNode tail = l;
+			while (l1 != null && l2 != null) {
+				ListNode node;
+				if (l1.val < l2.val) {
+					node = l1;
+					l1 = l1.next;
+
+				} else {
+					node = l2;
+					l2 = l2.next;
+				}
+				node.next = null;
+				if (l == null) {
+					l = node;
+					tail = node;
+				} else {
+					tail.next = node;
+					tail = node;
+				}
+			}
+			if (l1 != null) {
+				tail.next = l1;
+			} else {
+				tail.next = l2;
+			}
+			return l;
+		}
+	}
 }
