2123d04508cece1676e38a1a37f01558ad0c79c5
Fan Zhang <fredplusplus@gmail.com>
2013-09-08 17:10:52 -0700

    q45

--- src/JumpGame.java
+++ src/JumpGame.java
@@ -1,8 +1,8 @@
-/**
- * http://leetcode.com/onlinejudge#question_55
- * 
- */
 public class JumpGame {
+	/**
+	 * http://leetcode.com/onlinejudge#question_55
+	 * 
+	 */
 	public boolean canJump(int[] A) {
 		int canReachEnd = A.length - 1;
 		for (int i = A.length - 2; i >= 0; i--) {
@@ -11,5 +11,23 @@ public class JumpGame {
 			}
 		}
 		return canReachEnd == 0;
+	}
+
+	/**
+	 * http://leetcode.com/onlinejudge#question_45
+	 */
+	public int jump(int A[]) {
+		int ret = 0;
+		int last = 0;
+		int curr = 0;
+		for (int i = 0; i < A.length; ++i) {
+			if (i > last) {
+				last = curr;
+				++ret;
+			}
+			curr = Math.max(curr, i + A[i]);
+		}
+
+		return ret;
 	}
 }
