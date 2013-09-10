bac6ff6c9bf2e6bf79f380ca8557ec0ac3c829c4
Fan Zhang <fredplusplus@gmail.com>
2013-09-09 21:31:16 -0700

    q3

---
+++ src/LongestSubstringWithoutRepeat.java
@@ -0,0 +1,34 @@
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * http://leetcode.com/onlinejudge#question_3
+ * 
+ * 
+ */
+public class LongestSubstringWithoutRepeat {
+	public int lengthOfLongestSubstring(String s) {
+		int start = 0;
+		int end = 0;
+		int maxlength = 0;
+		Map<Character, Boolean> occurred = new HashMap<Character, Boolean>();
+		for (end = 0; end < s.length(); end++) {
+			if (occurred.get(s.charAt(end)) == null || occurred.get(s.charAt(end)) == false) {
+				occurred.put(s.charAt(end), true);
+			} else {
+				maxlength = Math.max(maxlength, end - start);
+				while (s.charAt(start) != s.charAt(end)) {
+					occurred.put(s.charAt(start++), false);
+				}
+				start++;
+			}
+		}
+		return Math.max(maxlength, end - start);
+	}
+
+	public static void main(String[] args) {
+		LongestSubstringWithoutRepeat rep = new LongestSubstringWithoutRepeat();
+
+		System.out.println(rep.lengthOfLongestSubstring("abcdcefcg"));
+	}
+}
