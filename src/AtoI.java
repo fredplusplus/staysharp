ed724552e0a2d4229bacb0c43ef22a6ddc208dff
Fan Zhang <fredplusplus@gmail.com>
2013-09-09 21:50:18 -0700

    q8

---
+++ src/AtoI.java
@@ -0,0 +1,66 @@
+/**
+ * http://leetcode.com/onlinejudge#question_8
+ * 
+ */
+public class AtoI {
+	public int atoi(String str) {
+		// Start typing your Java solution below
+		// DO NOT write main() function
+		if (str == null) {
+			return 0;
+		} else {
+			str = str.trim();
+			boolean valid = true;
+			boolean outOfRange = false;
+			int prev = 0;
+			int sign = 1;
+			int value = 0;
+			if (str.length() == 0) {
+				return 0;
+			}
+
+			for (int i = 0; i < str.length(); i++) {
+				if (i == 0) {
+					if (str.charAt(i) == '-') {
+						sign = -1;
+					} else if (str.charAt(i) == '+') {
+						sign = 1;
+					} else {
+						if (isDigit(str.charAt(i))) {
+							value = prev * 10 + convertDigit(str.charAt(i));
+							prev = value;
+						} else {
+							break;
+						}
+					}
+				} else {
+					if (isDigit(str.charAt(i))) {
+						value = prev * 10 + convertDigit(str.charAt(i));
+						if (value / 10 == prev) {
+							prev = value;
+						} else {
+							outOfRange = true;
+							break;
+						}
+					} else {
+						break;
+					}
+				}
+			}
+			if (outOfRange) {
+				return Integer.MAX_VALUE;
+			} else {
+				return value;
+			}
+		}
+
+	}
+
+	private boolean isDigit(char c) {
+		return convertDigit(c) >= 0 && convertDigit(c) <= 9;
+	}
+
+	private int convertDigit(char c) {
+		return c - '0';
+	}
+}
