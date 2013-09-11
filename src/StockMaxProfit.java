9f7465aaec56651026e83ee36d23803fd5af8145
Fan Zhang <fredplusplus@gmail.com>
2013-09-10 20:30:44 -0700

    q122

--- src/StockMaxProfit.java
+++ src/StockMaxProfit.java
@@ -1,11 +1,11 @@
-/**
- * http://leetcode.com/onlinejudge#question_121
- * 
- * @author fanzhang
- * 
- */
 public class StockMaxProfit {
-	public int maxProfit(int[] prices) {
+	/**
+	 * http://leetcode.com/onlinejudge#question_121
+	 * 
+	 * @author fanzhang
+	 * 
+	 */
+	public int maxProfit1(int[] prices) {
 		if (prices == null || prices.length < 2) {
 			return 0;
 		}
@@ -22,5 +22,22 @@ public class StockMaxProfit {
 			}
 		}
 		return maxDiff;
+	}
+
+	/**
+	 * http://leetcode.com/onlinejudge#question_122
+	 */
+	public int maxProfit(int[] prices) {
+		if (prices == null || prices.length <= 1) {
+			return 0;
+		}
+		int profit = 0;
+		for (int i = 1; i < prices.length; i++) {
+			int diff = prices[i] - prices[i - 1];
+			if (diff > 0) {
+				profit += diff;
+			}
+		}
+		return profit;
 	}
 }
