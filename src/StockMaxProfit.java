d8c19d94f119ee9a4142c1db56cc3cf768e28499
Fan Zhang <fredplusplus@gmail.com>
2013-09-10 21:08:27 -0700

    q123

--- src/StockMaxProfit.java
+++ src/StockMaxProfit.java
@@ -27,7 +27,7 @@ public class StockMaxProfit {
 	/**
 	 * http://leetcode.com/onlinejudge#question_122
 	 */
-	public int maxProfit(int[] prices) {
+	public int maxProfit2(int[] prices) {
 		if (prices == null || prices.length <= 1) {
 			return 0;
 		}
@@ -39,5 +39,43 @@ public class StockMaxProfit {
 			}
 		}
 		return profit;
+	}
+
+	/**
+	 * http://leetcode.com/onlinejudge#question_123
+	 * 
+	 */
+	public int maxProfit(int[] prices) {
+		if (prices == null || prices.length <= 1) {
+			return 0;
+		}
+		int[] backProfit = new int[prices.length];
+		int[] futureProfit = new int[prices.length];
+		int low = prices[0];
+		for (int i = 1; i < backProfit.length; i++) {
+			if (prices[i] < low) {
+				low = prices[i];
+				backProfit[i] = backProfit[i - 1];
+			} else {
+				int profit = prices[i] - low;
+				backProfit[i] = Math.max(backProfit[i - 1], profit);
+			}
+		}
+		int high = prices[prices.length - 1];
+		for (int i = futureProfit.length - 2; i >= 0; i--) {
+			if (prices[i] > high) {
+				high = prices[i];
+				futureProfit[i] = futureProfit[i + 1];
+			} else {
+				int profit = high - prices[i];
+				futureProfit[i] = Math.max(profit, futureProfit[i + 1]);
+			}
+		}
+		int maxProfit = 0;
+		for (int i = 0; i < prices.length; i++) {
+			int profit = backProfit[i] + futureProfit[i];
+			maxProfit = Math.max(maxProfit, profit);
+		}
+		return maxProfit;
 	}
 }
