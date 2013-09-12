272654f1c81d6a2e1286916d21135e818372d8fe
Fan Zhang <fredplusplus@gmail.com>
2013-09-11 21:56:43 -0700

    q29

---
+++ src/DivideTwoNumers.java
@@ -0,0 +1,34 @@
+public class DivideTwoNumers {
+	/**
+	 * http://leetcode.com/onlinejudge#question_29
+	 */
+	public int divide(int dividend, int divisor) {
+		long a = Math.abs((long) dividend);
+		long b = Math.abs((long) divisor);
+		boolean up = true;
+		int i = 0;
+		int ret = 0;
+		while (a >= b) {
+			long c = b << i;
+			if (a - c >= 0) {
+				a -= c;
+				ret += 1 << i;
+			}
+			if (up) {
+				i++;
+				if (a < c << 1) {
+					up = false;
+				}
+			} else {
+				i--;
+			}
+		}
+		return (dividend ^ divisor) >> 31 != 0 ? -ret : ret;
+	}
+
+	public static void main(String[] args) {
+		DivideTwoNumers dtn = new DivideTwoNumers();
+
+		System.out.println(dtn.divide(Integer.MIN_VALUE, Integer.MIN_VALUE));
+	}
+}
