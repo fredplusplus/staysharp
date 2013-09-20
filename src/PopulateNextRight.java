a1aa94f7ed29a01e052dcfb127b4f183fdcb544d
Fan Zhang <fredplusplus@gmail.com>
2013-09-19 20:21:40 -0700

    q117

--- src/PopulateNextRight.java
+++ src/PopulateNextRight.java
@@ -1,8 +1,10 @@
+import java.util.LinkedList;
+
 public class PopulateNextRight {
 	/**
 	 * http://leetcode.com/oldoj#question_116
 	 */
-	public void connect(TreeLinkNode root) {
+	public void connect1(TreeLinkNode root) {
 		TreeLinkNode leftFront = root;
 		while (leftFront != null) {
 			TreeLinkNode across = leftFront;
@@ -16,6 +18,39 @@ public class PopulateNextRight {
 				across = across.next;
 			}
 			leftFront = leftFront.left;
+		}
+	}
+
+	/**
+	 * http://leetcode.com/oldoj#question_117
+	 */
+	public void connect(TreeLinkNode root) {
+		LinkedList<TreeLinkNode> queue = new LinkedList<TreeLinkNode>();
+		if (root != null) {
+			queue.add(root);
+			queue.addLast(null);
+			TreeLinkNode prev = null;
+			while (!queue.isEmpty()) {
+				TreeLinkNode current = queue.removeFirst();
+				if (prev != null && current != null) {
+					prev.next = current;
+				}
+				prev = current;
+				if (current == null) {
+					if (queue.isEmpty()) {
+						break;
+					} else {
+						queue.addLast(null);
+					}
+				} else {
+					if (current.left != null) {
+						queue.addLast(current.left);
+					}
+					if (current.right != null) {
+						queue.addLast(current.right);
+					}
+				}
+			}
 		}
 	}
 }
