e7e6caa7fcc036bf3474591b386fcc78a9266480
Fan Zhang <fredplusplus@gmail.com>
2013-09-20 21:42:47 -0700

    q41

---
+++ src/FirstMissingPositive.java
@@ -0,0 +1,28 @@
+public class FirstMissingPositive {
+	/**
+	 * http://leetcode.com/oldoj#question_41
+	 */
+	public int firstMissingPositive(int[] A) {
+		int i = 0;
+		for (i = 0; i < A.length; i++) {
+			if (A[i] >= 1 && A[i] <= A.length && A[i] != i + 1 && A[i] != A[A[i] - 1]) {
+				int temp = A[i];
+				A[i] = A[temp - 1];
+				A[temp - 1] = temp;
+				i--;
+			}
+		}
+
+		for (i = 0; i < A.length; i++) {
+			if (A[i] != i + 1) {
+				return i + 1;
+			}
+		}
+		return i + 1;
+	}
+
+	public static void main(String[] args) {
+		FirstMissingPositive fm = new FirstMissingPositive();
+		System.out.println(fm.firstMissingPositive(new int[] { 3, 4, -1, 1 }));
+	}
+}
