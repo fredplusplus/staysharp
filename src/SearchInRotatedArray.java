1d6db60889729c23982c09c18568fb54b355e442
Fan Zhang <fredplusplus@gmail.com>
2013-09-21 15:22:46 -0700

    q81

---
+++ src/SearchInRotatedArray.java
@@ -0,0 +1,43 @@
+public class SearchInRotatedArray {
+	/**
+	 * http://leetcode.com/oldoj#question_81
+	 */
+	public boolean search(int[] A, int target) {
+		if (A == null || A.length == 0) {
+			return false;
+		} else {
+			return search(A, target, 0, A.length - 1);
+		}
+	}
+
+	private boolean search(int[] a, int target, int left, int right) {
+		if (left > right) {
+			return false;
+		}
+		int mid = left + (right - left) / 2;
+		if (a[mid] == target) {
+			return true;
+		} else {
+			boolean found = false;
+			if (a[mid] > a[left] && a[left] <= target && target < a[mid]) {
+				found = found || search(a, target, left, mid - 1);
+			}
+			if (!found && a[mid] < a[right] && a[mid] < target && target <= a[right]) {
+				found = search(a, target, mid + 1, right);
+			}
+			if (!found && a[mid] <= a[left]) {
+				found = found || search(a, target, left, mid - 1);
+			}
+			if (!found && a[mid] >= a[right]) {
+				found = search(a, target, mid + 1, right);
+			}
+			return found;
+		}
+	}
+
+	public static void main(String[] args) {
+		int[] A = new int[] { 1, 2, 3, 3, 4, 5, 6, 1 };
+		SearchInRotatedArray so = new SearchInRotatedArray();
+		System.out.println(so.search(A, 6));
+	}
+}
