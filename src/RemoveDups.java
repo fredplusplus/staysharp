163caf67cb47526a0e8fc93ba453d31a551be7d3
Fan Zhang <fredplusplus@gmail.com>
2013-09-20 20:18:35 -0700

    q82

--- src/RemoveDups.java
+++ src/RemoveDups.java
@@ -1,8 +1,11 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class RemoveDups {
 	/**
 	 * http://leetcode.com/oldoj#question_83
 	 */
-	public ListNode deleteDuplicates(ListNode head) {
+	public ListNode deleteDuplicates1(ListNode head) {
 		if (head == null) {
 			return null;
 		}
@@ -17,4 +20,41 @@ public class RemoveDups {
 		}
 		return head;
 	}
+
+	/**
+	 * http://leetcode.com/oldoj#question_82
+	 */
+	public ListNode deleteDuplicates(ListNode head) {
+		ListNode meta = new ListNode(0);
+		meta.next = head;
+		Map<ListNode, Boolean> dupeMap = new HashMap<ListNode, Boolean>();
+		while (head != null) {
+			if (head.next != null && head.next.val == head.val) {
+				head.next = head.next.next;
+				dupeMap.put(head, true);
+			} else {
+				head = head.next;
+			}
+		}
+		head = meta;
+		while (head.next != null) {
+			if (Boolean.TRUE.equals(dupeMap.get(head.next))) {
+				head.next = head.next.next;
+			} else {
+				head = head.next;
+			}
+		}
+		return meta.next;
+	}
+
+	public static void main(String[] args) {
+		RemoveDups rd = new RemoveDups();
+
+		ListNode head = new ListNode(1);
+		head.next = new ListNode(2);
+		head.next.next = new ListNode(2);
+		ListNode newHead = rd.deleteDuplicates(head);
+		System.out.println(newHead);
+	}
+
 }
