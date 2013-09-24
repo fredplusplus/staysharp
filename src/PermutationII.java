27d170a0d29ca1622df488f833b8bf90f3b5f67a
Fan Zhang <fredplusplus@gmail.com>
2013-09-23 19:57:39 -0700

    q47

---
+++ src/PermutationII.java
@@ -0,0 +1,42 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+
+public class PermutationII {
+	/**
+	 * http://leetcode.com/oldoj#question_47
+	 */
+	public ArrayList<ArrayList<Integer>> permuteUnique(int[] num) {
+		ArrayList<ArrayList<Integer>> set = new ArrayList<ArrayList<Integer>>();
+		boolean[] used = new boolean[num.length];
+		Arrays.sort(num);
+		LinkedList<Integer> path = new LinkedList<Integer>();
+		permute(num, used, path, set);
+		return set;
+	}
+
+	private void permute(int[] num, boolean[] used, LinkedList<Integer> path, ArrayList<ArrayList<Integer>> set) {
+		if (path.size() == num.length) {
+			ArrayList<Integer> res = new ArrayList<Integer>();
+			res.addAll(path);
+			set.add(res);
+		} else {
+			for (int i = 0; i < num.length; i++) {
+				if (used[i] || (i != 0 && num[i] == num[i - 1] && used[i - 1])) {
+					continue;
+				} else {
+					used[i] = true;
+					path.addLast(num[i]);
+					permute(num, used, path, set);
+					used[i] = false;
+					path.removeLast();
+				}
+			}
+		}
+	}
+
+	public static void main(String[] args) {
+		PermutationII per = new PermutationII();
+		System.out.println(per.permuteUnique(new int[] { 1, 1, 2, 3 }));
+	}
+}
