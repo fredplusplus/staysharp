91320651f8611d62b093aaa7c99842b9b2665b9b
Fan Zhang <fredplusplus@gmail.com>
2013-09-29 16:42:37 -0700

    three sum no duplicate

--- src/ThreeSumClosest.java
+++ src/ThreeSumClosest.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Arrays;
 
 public class ThreeSumClosest {
@@ -25,5 +26,37 @@ public class ThreeSumClosest {
 			}
 		}
 		return res;
+	}
+
+	public ArrayList<ArrayList<Integer>> threeSum(int[] num) {
+		ArrayList<ArrayList<Integer>> result = new ArrayList<ArrayList<Integer>>();
+		Arrays.sort(num);
+		for (int i = 0; i < num.length; i++) {
+			if (i > 0 && i < num.length && num[i] == num[i - 1]) {
+				i++;
+			}
+			int start = i + 1;
+			int end = num.length - 1;
+			while (start < end) {
+				if (num[i] + num[start] + num[end] == 0) {
+					ArrayList<Integer> line = new ArrayList<Integer>();
+					line.add(num[i]);
+					line.add(num[start]);
+					line.add(num[end]);
+					result.add(line);
+					do {
+						start++;
+					} while (start < end && num[start] == num[start - 1]);
+					do {
+						end--;
+					} while (start < end && num[end] == num[end + 1]);
+				} else if (num[i] + num[start] + num[end] > 0) {
+					end--;
+				} else {
+					start++;
+				}
+			}
+		}
+		return result;
 	}
 }
