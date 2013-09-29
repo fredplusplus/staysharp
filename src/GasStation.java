d9b54680ef333f112ad0c9f81af53834a597fc14
Fan Zhang <fredplusplus@gmail.com>
2013-09-29 11:54:08 -0700

    gas station

---
+++ src/GasStation.java
@@ -0,0 +1,40 @@
+public class GasStation {
+	/**
+	 * http://oj.leetcode.com/problems/gas-station/
+	 */
+	public int canCompleteCircuit(int[] gas, int[] cost) {
+		int[] netCost = new int[2 * gas.length];
+		for (int i = 0; i < gas.length; i++) {
+			netCost[i] = gas[i] - cost[i];
+		}
+		for (int i = gas.length; i < 2 * gas.length; i++) {
+			netCost[i] = netCost[i - gas.length];
+		}
+		int startPoint = 0;
+		int count = 0;
+		int tank = 0;
+		for (int i = 0; i < netCost.length; i++) {
+			tank += netCost[i];
+			if (tank < 0) {
+				count = 0;
+				tank = 0;
+				startPoint = i + 1;
+			} else {
+				count++;
+				if (count == gas.length) {
+					break;
+				}
+			}
+		}
+		if (count == gas.length) {
+			return startPoint;
+		} else {
+			return -1;
+		}
+	}
+
+	public static void main(String[] args) {
+		GasStation gs = new GasStation();
+		System.out.println(gs.canCompleteCircuit(new int[] { 0, 2, 1, 1 }, new int[] { 1, 0, 2, 1 }));
+	}
+}
