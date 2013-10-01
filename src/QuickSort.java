8c6fe4393b44e8090fb42da3e83939c029ac3248
Fan Zhang <fredplusplus@gmail.com>
2013-09-30 22:10:28 -0700

    rewrote quick sort

--- src/QuickSort.java
+++ src/QuickSort.java
@@ -7,29 +7,20 @@ public class QuickSort {
 		if (start >= end) {
 			return;
 		} else {
-			int pivot = partition(num, start, end);
-			sort(num, start, pivot - 1);
-			sort(num, pivot + 1, end);
-		}
-	}
-
-	private int partition(int[] num, int start, int end) {
-		if (start >= end) {
-			return start;
-		} else {
 			int pivot = num[end];
 			int index = start;
 			for (int i = start; i < end; i++) {
 				if (num[i] < pivot) {
-					int temp = num[index];
-					num[index++] = num[i];
-					num[i] = temp;
+					int temp = num[i];
+					num[i] = num[index];
+					num[index++] = temp;
 				}
 			}
 			int temp = num[index];
 			num[index] = num[end];
 			num[end] = temp;
-			return index;
+			sort(num, start, index - 1);
+			sort(num, index + 1, end);
 		}
 	}
 }
